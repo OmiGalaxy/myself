4199820453b145c4977c1767298323f78d2bc143
Om Bhosale <142747405+OmiGalaxy@users.noreply.github.com>
2025-03-21 20:56:22 +0530

    Add files via upload

---
+++ src/components/Main.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import '../assets/styles/Main.scss';
+
+function Main() {
+
+  return (
+    <div className="container">
+      <div className="about-section">
+        <div className="content">
+          <div className="social_icons">
+            <a href="https://github.com/TheAnuja" target="_blank" rel="noreferrer"><GitHubIcon/></a>
+            <a href="https://www.linkedin.com/in/anuja-patil-9804b325b/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
+          </div>
+          <h1>Anuja Patil</h1>
+          <p>Electronics And Telecommunication Engineer</p>
+
+          <div className="mobile_social_icons">
+            <a href="https://github.com/TheAnuja" target="_blank" rel="noreferrer"><GitHubIcon/></a>
+            <a href="https://www.linkedin.com/in/anuja-patil-9804b325b/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Main;
